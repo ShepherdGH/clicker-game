f934b83ca86655bdaa6f24b0d8609b45be47f2d3
ShepherdGH <yuxinmiao222@gmail.com>
2025-08-02 21:47:06 -0400

    added service api

---
+++ client/src/services/api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:5000/api';
+
+// Generate/Get User ID, currently using localStorage which is not safe
+const getUserId = () => {
+  let userId = localStorage.getItem('userId');
+  if (!userId) {
+    userId = 'user_' + Math.random().toString(36).substr(2, 9);
+    localStorage.setItem('userId', userId);
+  }
+  return userId;
+};
+
+const api = {
+  getGameData: async () => {
+    const response = await axios.get(`${API_BASE_URL}/game/${getUserId()}`);
+    return response.data;
+  },
+  
+  click: async () => {
+    const response = await axios.post(`${API_BASE_URL}/game/${getUserId()}/click`);
+    return response.data;
+  },
+  
+  buyUpgrade: async (upgradeType) => {
+    const response = await axios.post(`${API_BASE_URL}/game/${getUserId()}/upgrade`, {
+      upgradeType
+    });
+    return response.data;
+  }
+};
+
+export default api;
